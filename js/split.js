c1191094cc08f1c0ecbb7123ab9b4bb329eaebd0
amin367 <aganji367@gmail.com>
2021-07-10 18:21:48 +0430

    Upload

---
+++ js/split.js
@@ -0,0 +1,7 @@
+let email = "aganji367@gmail.com";
+let emailSplit = email.split("@");
+let stringOfEmailSplited = emailSplit.toString();
+let emailSplit2 = stringOfEmailSplited.split(".");
+let string = emailSplit2.toString();
+
+console.log(string);
